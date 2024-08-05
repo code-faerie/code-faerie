d9730279d871f2f26357f68b59813fae0ad2c321
code-faerie <146558721+code-faerie@users.noreply.github.com>
2024-08-05 14:49:16 +0700

    wip: tailwindcss

--- Gemfile
+++ Gemfile
@@ -1,5 +1,4 @@
 source "https://rubygems.org"
 
-gem "github-pages", "~> 231", group: :jekyll_plugins
+gem "jekyll"
 gem "webrick"
-gem "jekyll-postcss"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: [
-    './_drafts/**/*.html',
     './_includes/**/*.html',
     './_layouts/**/*.html',
     './_posts/*.md',
-    './*.md',
     './*.html',
   ],
   theme: {
-    theme: {
-      extend: {},
-    },
+    extend: {},
   },
-  plugins: []
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
